c31b8064126519fd54d624202af7a91ec4b9e766
Senon Silvestre <42272961+senjaymac@users.noreply.github.com>
2022-06-30 17:37:01 +0800

    When Text is filled it can't be replaced anymore

--- VB TIC TAC TOE/VB-Tic-Tac-Toe/VB-Tic-Tac-Toe/Form1.vb
+++ VB TIC TAC TOE/VB-Tic-Tac-Toe/VB-Tic-Tac-Toe/Form1.vb
@@ -2,91 +2,112 @@
     Dim Turn As Char = "X"
 
     Private Sub leftX_Click(sender As Object, e As EventArgs) Handles leftX.Click
-        leftX.Text = Turn
-        If Turn = "X" Then
-            Turn = "O"
-        Else
-            Turn = "X"
+        If leftX.Text = "" Then
+            leftX.Text = Turn
+
+            If Turn = "X" Then
+                Turn = "O"
+            Else
+                Turn = "X"
+            End If
         End If
 
     End Sub
 
     Private Sub midX_Click(sender As Object, e As EventArgs) Handles midX.Click
-        midX.Text = Turn
-        If Turn = "X" Then
-            Turn = "O"
-        Else
-            Turn = "X"
+        If midX.Text = "" Then
+            midX.Text = Turn
+
+            If Turn = "X" Then
+                Turn = "O"
+            Else
+                Turn = "X"
+            End If
         End If
 
     End Sub
 
     Private Sub rightX_Click(sender As Object, e As EventArgs) Handles rightX.Click
-        rightX.Text = Turn
-        If Turn = "X" Then
-            Turn = "O"
-        Else
-            Turn = "X"
+        If rightX.Text = "" Then
+            rightX.Text = Turn
+
+            If Turn = "X" Then
+                Turn = "O"
+            Else
+                Turn = "X"
+            End If
         End If
 
     End Sub
 
     Private Sub leftY_Click(sender As Object, e As EventArgs) Handles leftY.Click
-        leftY.Text = Turn
-        If Turn = "X" Then
-            Turn = "O"
-        Else
-            Turn = "X"
+        If leftY.Text = "" Then
+            leftY.Text = Turn
+            If Turn = "X" Then
+                Turn = "O"
+            Else
+                Turn = "X"
+            End If
         End If
 
     End Sub
 
     Private Sub midY_Click(sender As Object, e As EventArgs) Handles midY.Click
-        midY.Text = Turn
-        If Turn = "X" Then
-            Turn = "O"
-        Else
-            Turn = "X"
+        If midY.Text = "" Then
+            midY.Text = Turn
+            If Turn = "X" Then
+                Turn = "O"
+            Else
+                Turn = "X"
+            End If
         End If
 
     End Sub
 
     Private Sub rightY_Click(sender As Object, e As EventArgs) Handles rightY.Click
-        rightY.Text = Turn
-        If Turn = "X" Then
-            Turn = "O"
-        Else
-            Turn = "X"
+        If rightY.Text = "" Then
+            rightY.Text = Turn
+            If Turn = "X" Then
+                Turn = "O"
+            Else
+                Turn = "X"
+            End If
         End If
 
     End Sub
 
     Private Sub leftZ_Click(sender As Object, e As EventArgs) Handles leftZ.Click
-        leftZ.Text = Turn
-        If Turn = "X" Then
-            Turn = "O"
-        Else
-            Turn = "X"
+        If leftZ.Text = "" Then
+            leftZ.Text = Turn
+            If Turn = "X" Then
+                Turn = "O"
+            Else
+                Turn = "X"
+            End If
         End If
 
     End Sub
 
     Private Sub midZ_Click(sender As Object, e As EventArgs) Handles midZ.Click
-        midZ.Text = Turn
-        If Turn = "X" Then
-            Turn = "O"
-        Else
-            Turn = "X"
+        If midZ.Text = "" Then
+            midZ.Text = Turn
+            If Turn = "X" Then
+                Turn = "O"
+            Else
+                Turn = "X"
+            End If
         End If
 
     End Sub
 
     Private Sub rightZ_Click(sender As Object, e As EventArgs) Handles rightZ.Click
-        rightZ.Text = Turn
-        If Turn = "X" Then
-            Turn = "O"
-        Else
-            Turn = "X"
+        If rightZ.Text = "" Then
+            rightZ.Text = Turn
+            If Turn = "X" Then
+                Turn = "O"
+            Else
+                Turn = "X"
+            End If
         End If
 
     End Sub
