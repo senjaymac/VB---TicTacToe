f607f66fada3d90e9ca74f5b8a15cb4a2a032b4e
Senon Silvestre <42272961+senjaymac@users.noreply.github.com>
2022-06-30 17:18:23 +0800

    Buttons can now register X and O

--- VB TIC TAC TOE/VB-Tic-Tac-Toe/VB-Tic-Tac-Toe/Form1.Designer.vb
+++ VB TIC TAC TOE/VB-Tic-Tac-Toe/VB-Tic-Tac-Toe/Form1.Designer.vb
@@ -22,121 +22,112 @@ Partial Class baseForm
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.Button1 = New System.Windows.Forms.Button()
-        Me.Button2 = New System.Windows.Forms.Button()
-        Me.Button3 = New System.Windows.Forms.Button()
-        Me.Button4 = New System.Windows.Forms.Button()
-        Me.Button5 = New System.Windows.Forms.Button()
-        Me.Button6 = New System.Windows.Forms.Button()
-        Me.Button7 = New System.Windows.Forms.Button()
-        Me.Button8 = New System.Windows.Forms.Button()
-        Me.Button9 = New System.Windows.Forms.Button()
+        Me.leftX = New System.Windows.Forms.Button()
+        Me.midX = New System.Windows.Forms.Button()
+        Me.rightX = New System.Windows.Forms.Button()
+        Me.leftY = New System.Windows.Forms.Button()
+        Me.midY = New System.Windows.Forms.Button()
+        Me.rightY = New System.Windows.Forms.Button()
+        Me.leftZ = New System.Windows.Forms.Button()
+        Me.midZ = New System.Windows.Forms.Button()
+        Me.rightZ = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
-        'Button1
+        'leftX
         '
-        Me.Button1.Font = New System.Drawing.Font("Comic Sans MS", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Button1.Location = New System.Drawing.Point(50, 35)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(141, 109)
-        Me.Button1.TabIndex = 0
-        Me.Button1.Text = "Button1"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.leftX.Font = New System.Drawing.Font("Comic Sans MS", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.leftX.Location = New System.Drawing.Point(50, 35)
+        Me.leftX.Name = "leftX"
+        Me.leftX.Size = New System.Drawing.Size(141, 109)
+        Me.leftX.TabIndex = 0
+        Me.leftX.UseVisualStyleBackColor = True
         '
-        'Button2
+        'midX
         '
-        Me.Button2.Font = New System.Drawing.Font("Comic Sans MS", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Button2.Location = New System.Drawing.Point(197, 35)
-        Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(141, 109)
-        Me.Button2.TabIndex = 1
-        Me.Button2.Text = "Button2"
-        Me.Button2.UseVisualStyleBackColor = True
+        Me.midX.Font = New System.Drawing.Font("Comic Sans MS", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.midX.Location = New System.Drawing.Point(197, 35)
+        Me.midX.Name = "midX"
+        Me.midX.Size = New System.Drawing.Size(141, 109)
+        Me.midX.TabIndex = 1
+        Me.midX.UseVisualStyleBackColor = True
         '
-        'Button3
+        'rightX
         '
-        Me.Button3.Font = New System.Drawing.Font("Comic Sans MS", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Button3.Location = New System.Drawing.Point(344, 35)
-        Me.Button3.Name = "Button3"
-        Me.Button3.Size = New System.Drawing.Size(141, 109)
-        Me.Button3.TabIndex = 2
-        Me.Button3.Text = "Button3"
-        Me.Button3.UseVisualStyleBackColor = True
+        Me.rightX.Font = New System.Drawing.Font("Comic Sans MS", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.rightX.Location = New System.Drawing.Point(344, 35)
+        Me.rightX.Name = "rightX"
+        Me.rightX.Size = New System.Drawing.Size(141, 109)
+        Me.rightX.TabIndex = 2
+        Me.rightX.UseVisualStyleBackColor = True
         '
-        'Button4
+        'leftY
         '
-        Me.Button4.Font = New System.Drawing.Font("Comic Sans MS", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Button4.Location = New System.Drawing.Point(50, 150)
-        Me.Button4.Name = "Button4"
-        Me.Button4.Size = New System.Drawing.Size(141, 109)
-        Me.Button4.TabIndex = 3
-        Me.Button4.Text = "Button4"
-        Me.Button4.UseVisualStyleBackColor = True
+        Me.leftY.Font = New System.Drawing.Font("Comic Sans MS", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.leftY.Location = New System.Drawing.Point(50, 150)
+        Me.leftY.Name = "leftY"
+        Me.leftY.Size = New System.Drawing.Size(141, 109)
+        Me.leftY.TabIndex = 3
+        Me.leftY.UseVisualStyleBackColor = True
         '
-        'Button5
+        'midY
         '
-        Me.Button5.Font = New System.Drawing.Font("Comic Sans MS", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Button5.Location = New System.Drawing.Point(197, 150)
-        Me.Button5.Name = "Button5"
-        Me.Button5.Size = New System.Drawing.Size(141, 109)
-        Me.Button5.TabIndex = 4
-        Me.Button5.Text = "Button5"
-        Me.Button5.UseVisualStyleBackColor = True
+        Me.midY.Font = New System.Drawing.Font("Comic Sans MS", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.midY.Location = New System.Drawing.Point(197, 150)
+        Me.midY.Name = "midY"
+        Me.midY.Size = New System.Drawing.Size(141, 109)
+        Me.midY.TabIndex = 4
+        Me.midY.UseVisualStyleBackColor = True
         '
-        'Button6
+        'rightY
         '
-        Me.Button6.Font = New System.Drawing.Font("Comic Sans MS", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Button6.Location = New System.Drawing.Point(344, 150)
-        Me.Button6.Name = "Button6"
-        Me.Button6.Size = New System.Drawing.Size(141, 109)
-        Me.Button6.TabIndex = 5
-        Me.Button6.Text = "Button6"
-        Me.Button6.UseVisualStyleBackColor = True
+        Me.rightY.Font = New System.Drawing.Font("Comic Sans MS", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.rightY.Location = New System.Drawing.Point(344, 150)
+        Me.rightY.Name = "rightY"
+        Me.rightY.Size = New System.Drawing.Size(141, 109)
+        Me.rightY.TabIndex = 5
+        Me.rightY.UseVisualStyleBackColor = True
         '
-        'Button7
+        'leftZ
         '
-        Me.Button7.Font = New System.Drawing.Font("Comic Sans MS", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Button7.Location = New System.Drawing.Point(50, 265)
-        Me.Button7.Name = "Button7"
-        Me.Button7.Size = New System.Drawing.Size(141, 109)
-        Me.Button7.TabIndex = 6
-        Me.Button7.Text = "Button7"
-        Me.Button7.UseVisualStyleBackColor = True
+        Me.leftZ.Font = New System.Drawing.Font("Comic Sans MS", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.leftZ.Location = New System.Drawing.Point(50, 265)
+        Me.leftZ.Name = "leftZ"
+        Me.leftZ.Size = New System.Drawing.Size(141, 109)
+        Me.leftZ.TabIndex = 6
+        Me.leftZ.UseVisualStyleBackColor = True
         '
-        'Button8
+        'midZ
         '
-        Me.Button8.Font = New System.Drawing.Font("Comic Sans MS", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Button8.Location = New System.Drawing.Point(197, 265)
-        Me.Button8.Name = "Button8"
-        Me.Button8.Size = New System.Drawing.Size(141, 109)
-        Me.Button8.TabIndex = 7
-        Me.Button8.Text = "Button8"
-        Me.Button8.UseVisualStyleBackColor = True
+        Me.midZ.Font = New System.Drawing.Font("Comic Sans MS", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.midZ.Location = New System.Drawing.Point(197, 265)
+        Me.midZ.Name = "midZ"
+        Me.midZ.Size = New System.Drawing.Size(141, 109)
+        Me.midZ.TabIndex = 7
+        Me.midZ.UseVisualStyleBackColor = True
         '
-        'Button9
+        'rightZ
         '
-        Me.Button9.Font = New System.Drawing.Font("Comic Sans MS", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Button9.Location = New System.Drawing.Point(344, 265)
-        Me.Button9.Name = "Button9"
-        Me.Button9.Size = New System.Drawing.Size(141, 109)
-        Me.Button9.TabIndex = 8
-        Me.Button9.Text = "Button9"
-        Me.Button9.UseVisualStyleBackColor = True
+        Me.rightZ.Font = New System.Drawing.Font("Comic Sans MS", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.rightZ.Location = New System.Drawing.Point(344, 265)
+        Me.rightZ.Name = "rightZ"
+        Me.rightZ.Size = New System.Drawing.Size(141, 109)
+        Me.rightZ.TabIndex = 8
+        Me.rightZ.UseVisualStyleBackColor = True
         '
         'baseForm
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(556, 414)
-        Me.Controls.Add(Me.Button9)
-        Me.Controls.Add(Me.Button8)
-        Me.Controls.Add(Me.Button7)
-        Me.Controls.Add(Me.Button6)
-        Me.Controls.Add(Me.Button5)
-        Me.Controls.Add(Me.Button4)
-        Me.Controls.Add(Me.Button3)
-        Me.Controls.Add(Me.Button2)
-        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.rightZ)
+        Me.Controls.Add(Me.midZ)
+        Me.Controls.Add(Me.leftZ)
+        Me.Controls.Add(Me.rightY)
+        Me.Controls.Add(Me.midY)
+        Me.Controls.Add(Me.leftY)
+        Me.Controls.Add(Me.rightX)
+        Me.Controls.Add(Me.midX)
+        Me.Controls.Add(Me.leftX)
         Me.MaximizeBox = False
         Me.Name = "baseForm"
         Me.ShowIcon = False
@@ -145,13 +136,13 @@ Partial Class baseForm
 
     End Sub
 
-    Friend WithEvents Button1 As Button
-    Friend WithEvents Button2 As Button
-    Friend WithEvents Button3 As Button
-    Friend WithEvents Button4 As Button
-    Friend WithEvents Button5 As Button
-    Friend WithEvents Button6 As Button
-    Friend WithEvents Button7 As Button
-    Friend WithEvents Button8 As Button
-    Friend WithEvents Button9 As Button
+    Friend WithEvents leftX As Button
+    Friend WithEvents midX As Button
+    Friend WithEvents rightX As Button
+    Friend WithEvents leftY As Button
+    Friend WithEvents midY As Button
+    Friend WithEvents rightY As Button
+    Friend WithEvents leftZ As Button
+    Friend WithEvents midZ As Button
+    Friend WithEvents rightZ As Button
 End Class
--- VB TIC TAC TOE/VB-Tic-Tac-Toe/VB-Tic-Tac-Toe/Form1.vb
+++ VB TIC TAC TOE/VB-Tic-Tac-Toe/VB-Tic-Tac-Toe/Form1.vb
@@ -1,7 +1,95 @@
 ﻿Public Class baseForm
     Dim Turn As Char = "X"
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        Button1.Text = Turn
+
+    Private Sub leftX_Click(sender As Object, e As EventArgs) Handles leftX.Click
+        leftX.Text = Turn
+        If Turn = "X" Then
+            Turn = "O"
+        Else
+            Turn = "X"
+        End If
+
     End Sub
+
+    Private Sub midX_Click(sender As Object, e As EventArgs) Handles midX.Click
+        midX.Text = Turn
+        If Turn = "X" Then
+            Turn = "O"
+        Else
+            Turn = "X"
+        End If
+
+    End Sub
+
+    Private Sub rightX_Click(sender As Object, e As EventArgs) Handles rightX.Click
+        rightX.Text = Turn
+        If Turn = "X" Then
+            Turn = "O"
+        Else
+            Turn = "X"
+        End If
+
+    End Sub
+
+    Private Sub leftY_Click(sender As Object, e As EventArgs) Handles leftY.Click
+        leftY.Text = Turn
+        If Turn = "X" Then
+            Turn = "O"
+        Else
+            Turn = "X"
+        End If
+
+    End Sub
+
+    Private Sub midY_Click(sender As Object, e As EventArgs) Handles midY.Click
+        midY.Text = Turn
+        If Turn = "X" Then
+            Turn = "O"
+        Else
+            Turn = "X"
+        End If
+
+    End Sub
+
+    Private Sub rightY_Click(sender As Object, e As EventArgs) Handles rightY.Click
+        rightY.Text = Turn
+        If Turn = "X" Then
+            Turn = "O"
+        Else
+            Turn = "X"
+        End If
+
+    End Sub
+
+    Private Sub leftZ_Click(sender As Object, e As EventArgs) Handles leftZ.Click
+        leftZ.Text = Turn
+        If Turn = "X" Then
+            Turn = "O"
+        Else
+            Turn = "X"
+        End If
+
+    End Sub
+
+    Private Sub midZ_Click(sender As Object, e As EventArgs) Handles midZ.Click
+        midZ.Text = Turn
+        If Turn = "X" Then
+            Turn = "O"
+        Else
+            Turn = "X"
+        End If
+
+    End Sub
+
+    Private Sub rightZ_Click(sender As Object, e As EventArgs) Handles rightZ.Click
+        rightZ.Text = Turn
+        If Turn = "X" Then
+            Turn = "O"
+        Else
+            Turn = "X"
+        End If
+
+    End Sub
+
 
 End Class
